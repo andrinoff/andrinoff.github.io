8982a0c93234cc7a34479ebe489b40b90b1f7a72
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-06-07 17:21:41 +0400

    init

---
+++ src/links.svelte
@@ -0,0 +1,74 @@
+<script>
+	import Link from "./link.svelte";
+	import LinkMob from "./link Mob.svelte";
+</script>
+
+<div id="yes">
+	<Link
+		link="#/"
+		text="Home"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/4c0390f6c9a91b83f3ae6da9d5b5a75181472fd0_home_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
+	/>
+	<Link
+		link="#/aboutme"
+		text="About"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/978be0e162f6aebd63c1237006f065030a90264e_person_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
+	/>
+	<Link
+		link="#/contactme"
+		text="Contact"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/fc3f312e696ec561d201c3ffe0874df1e09e70f0_waving_hand_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
+	/>
+	<Link
+		link="#/Projects"
+		text="Projects"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/7b8b6bf02ed987b0e68ce5c2814ea408d666caf5_folder_code_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
+	/>
+	<Link
+		link="https://github.com/andrinoff"
+		text="Github"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/f202b6faccfd5cc46299b976c2635fee60b55aa0_github-mark-white.svg"
+	/>
+</div>
+<div id="no">
+	<LinkMob
+		link="#/"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/4c0390f6c9a91b83f3ae6da9d5b5a75181472fd0_home_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
+	/>
+	<LinkMob
+		link="#/aboutme"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/978be0e162f6aebd63c1237006f065030a90264e_person_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
+	/>
+	<LinkMob
+		link="#/contactme"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/fc3f312e696ec561d201c3ffe0874df1e09e70f0_waving_hand_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
+	/>
+	<LinkMob
+		link="#/Projects"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/7b8b6bf02ed987b0e68ce5c2814ea408d666caf5_folder_code_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
+	/>
+	<LinkMob
+		link="https://github.com/RezHackXYZ"
+		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/f202b6faccfd5cc46299b976c2635fee60b55aa0_github-mark-white.svg"
+	/>
+</div>
+
+<style>
+	#yes {
+		display: flex;
+	}
+
+	#no {
+		display: none;
+	}
+
+	@media screen and (max-width: 800px) {
+		#yes {
+			display: none;
+		}
+
+		#no {
+			display: flex;
+		}
+	}
+</style>
---
+++ src/pages/projects/projects.svelte
@@ -0,0 +1,132 @@
+<script>
+	let Projects = [
+		{
+			Name: "Personal Website",
+			Description: "This awesome website about me and my projects!",
+			Image: "https://i.imgur.com/5zHfTeQ.png",
+			Images: ["Javascript", "svelte", "vite"],
+			Link: "https://andrinoff.github.io",
+		},
+		{
+			Name: "Work Progress",
+			Description:
+				"A VS Code extension that shows your work progress in a nice way.",
+			Image: "https://github.com/work-progress/assets/raw/master/banners/banner-work-progress-white.png?raw=true",
+			Images: ["typescript", "nextdotjs", "tailwindcss"],
+			Link: "https://github.com/work-progress",
+		},
+		{
+			Name: "Anglobotik",
+			Description:
+				"A Telegram bot that helps you mock English writing exam",
+			Image: "https://github.com/andrinoff/anglobotik/raw/main/docs/icon.jpg",
+			Images: ["python", "telegram", "openai"],
+			Link: "https://github.com/andrinoff/anglobotik",
+		},
+		{
+			Name: "Currency Converter",
+			Description:
+				"A Telegram bot with multi-language support",
+			Image: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/e060bcaf74865f953fed5e88ae5f24020598d814_image.png",
+			Images: ["python", "sqlite", "telegram"],
+			Link: "https://github.com/andrinoff/TelegramCurrencyConverter",
+		},
+	];
+</script>
+
+<div id="root">
+	<h1>My Projects</h1>
+	<div id="grid">
+		{#each Projects as Project}
+			<div class="project-card">
+				<h2>{Project.Name}</h2>
+				<p>{@html Project.Description}</p>
+				<div class="image-gallery">
+					{#each Project.Images as Image}
+						<img
+							class="gallery-image"
+							alt="Project Tech Stack"
+							src="https://cdn.simpleicons.org/{Image}/grey"
+						/>
+					{/each}
+				</div>
+				<div class ="image-cover">
+				<img class="main-image" src={Project.Image} alt="Project" />
+				</div>
+				<a href={Project.Link} target="_blank"
+					><button class="ViewProject"> Open Project </button></a
+				>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	#root {
+		display: flex;
+		flex-direction: column;
+		margin: 0;
+		align-items: center;
+	}
+	h2,
+	p {
+		margin: 0;
+	}
+
+	p {
+		color: #818181;
+	}
+	#grid {
+		display: flex;
+		flex-wrap: wrap;
+		max-width: 1200px;
+		justify-content: center;
+	}
+
+	.project-card {
+		border: 3px solid #333;
+		width: 250px;
+		margin: 10px;
+		padding: 5px;
+		padding-bottom: 0px;
+	}
+	.image-gallery {
+		display: flex;
+		gap: 10px;
+		margin-top: 10px;
+		margin-bottom: 10px;
+	}
+	.gallery-image {
+		width: 30px;
+		height: 30px;
+		object-fit: cover;
+	}
+	.image-cover {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 10px;
+	}
+	.main-image {
+		height: 146px;
+		max-width: 246px;
+		text-align: center;
+		border: 2px solid #2e2e2e;
+	}
+	.ViewProject {
+		background-color: #242424;
+		color: white;
+		border: none;
+		margin-bottom: 5px;
+		padding: 10px;
+		width: 100%;
+		cursor: pointer;
+		font-size: 16px;
+		font-family: "Funnel Display", sans-serif;
+		transition: all 0.3s ease;
+	}
+	.ViewProject:hover {
+		background-color: #333;
+		transform: scale(1.05) rotateZ(-5deg);
+	}
+</style>
