f2d7a8cc53430b0a0248697a43a83ad7869e530b
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-06-10 19:31:47 +0400

    feat: about me experience changed

--- src/pages/about-me/aboutme.svelte
+++ src/pages/about-me/aboutme.svelte
@@ -1,67 +1,69 @@
 <div id="root">
-	<h1>About Me</h1>
-
-	
-
-	<h2> Education:</h2>
+	<h2 class="text-2xl"> Education:</h2>
 	<br />
 		<p>
 			2024-2026: A-levels </p>
 		<p id ="lower"> CS, Math, Econ at Projector School.</p>
 		
 	<h2>Experience:</h2>
-	<br />
-	<p>
-		2022-present: Freelance Software Engineer.
-	</p>
-	<p id="lower">
-		Developed web application and websites for various clients, including Dominos in Tbilisi, Georgia.
-	</p><br />
-	<p>
-		2023-2024: Studio Fullstack Developer
-	</p> 
-	<p id="lower">
-		Built websites for various clients, including a websites for a local restaurants in Tbilisi, Georgia.
-	</p><br />
-		<p>
-		2024-2025: Backend Developer in freemoviesfull.cc
-	</p>
-	<p id="lower">
-		Built backend for a movie streaming website, including video streaming, and database management.
-	</p>
+	<div class="grid grid-cols-[40px_1fr] gap-x-2 px-4">
+            <div class="flex flex-col items-center gap-1 pt-3">
+            <div class="text-[#101418]" data-icon="Buildings" data-size="24px" data-weight="regular">
+                <svg width="20" height="20">
+				<circle cx="10" cy="10" r="5" fill="white" />
+				</svg>
+            </div>
+            <div class="w-[1.5px] bg-[#d4dbe2] h-2 grow"></div>
+            </div>
+            <div class="flex flex-1 flex-col py-3">
+            <p class="text-[#fff] text-base font-medium leading-normal">Studio Fullstack Engineer</p>
+            <p class="text-[#5c738a] text-base font-normal leading-normal">2023-2024</p>
+            </div>
+            <div class="flex flex-col items-center gap-1">
+            <div class="w-[1.5px] bg-[#d4dbe2] h-2"></div>
+            <div class="text-[#101418]" data-icon="RocketLaunch" data-size="24px" data-weight="regular">
+                <svg width="20" height="20">
+				<circle cx="10" cy="10" r="5" fill="white" />
+				</svg>	
+            </div>
+            <div class=" bg-[#d4dbe2] h-2 grow"></div>
+            </div>
+            <div class="flex flex-1 flex-col py-3">
+            <p class="text-[#fff] text-base font-medium leading-normal">Freemoviesfull.cc Backend Engineer</p>
+            <p class="text-[#5c738a] text-base font-normal leading-normal">2024-2025</p>
+            </div>
+        </div>
+		
+		
 
-		<h2 id="-tech-stack-">💻 Tech Stack:</h2> 
-		<br />
-		<div class="badge-grid">
-		<img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&amp;logo=csharp&amp;logoColor=white" alt="C#"> <img src="https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&amp;logo=go&amp;logoColor=white" alt="Go"> <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&amp;logo=html5&amp;logoColor=white" alt="HTML5"> <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&amp;logo=javascript&amp;logoColor=%23F7DF1E" alt="JavaScript"> <img src="https://img.shields.io/badge/lua-%232C2D72.svg?style=for-the-badge&amp;logo=lua&amp;logoColor=white" alt="Lua"> <img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&amp;logo=python&amp;logoColor=ffdd54" alt="Python"> <img src="https://img.shields.io/badge/rust-%23000000.svg?style=for-the-badge&amp;logo=rust&amp;logoColor=white" alt="Rust"> <img src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&amp;logo=typescript&amp;logoColor=white" alt="TypeScript"> <img src="https://img.shields.io/badge/bootstrap-%238511FA.svg?style=for-the-badge&amp;logo=bootstrap&amp;logoColor=white" alt="Bootstrap"> <img src="https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&amp;logo=django&amp;logoColor=white" alt="Django"> <img src="https://img.shields.io/badge/Electron-191970?style=for-the-badge&amp;logo=Electron&amp;logoColor=white" alt="Electron.js"> <img src="https://img.shields.io/badge/expo-1C1E24?style=for-the-badge&amp;logo=expo&amp;logoColor=#D04A37" alt="Expo"> <img src="https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&amp;logo=Flutter&amp;logoColor=white" alt="Flutter"> <img src="https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&amp;logo=dart&amp;logoColor=white" alt="Dart"> <img src="https://img.shields.io/badge/Next-black?style=for-the-badge&amp;logo=next.js&amp;logoColor=white" alt="Next JS"> <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&amp;logo=node.js&amp;logoColor=white" alt="NodeJS"> <img src="https://img.shields.io/badge/NPM-%23CB3837.svg?style=for-the-badge&amp;logo=npm&amp;logoColor=white" alt="NPM"> <img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&amp;logo=react&amp;logoColor=%2361DAFB" alt="React"> <img src="https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&amp;logo=react&amp;logoColor=%2361DAFB" alt="React Native"> <img src="https://img.shields.io/badge/mysql-4479A1.svg?style=for-the-badge&amp;logo=mysql&amp;logoColor=white" alt="MySQL"> <img src="https://img.shields.io/badge/sqlite-%2307405e.svg?style=for-the-badge&amp;logo=sqlite&amp;logoColor=white" alt="SQLite"> <img src="https://img.shields.io/badge/redis-%23DD0031.svg?style=for-the-badge&amp;logo=redis&amp;logoColor=white" alt="Redis"></div>
-		<br />
+
 
 </div>
 
 <style>
+	h2 {
+		font-style: bold;
+		color: aliceblue;
+		font-size: larger;
+	}
 	* {
 		margin: 0;
 	}
-	.badge-grid {
-	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(400px, 3fr));
-	gap: 10px;
-	padding: 20px;
-	max-width: 200px;
-}
 	#root {
 		display: flex;
 		flex-direction: column;
-		margin: 0;
+		
 		align-items: center;
 	}
 
 	h2 {
 		margin-top: 20px;
-		margin-bottom: 5px;
+		
 	}
 	#lower {
 		margin: 0px;
 		color: #818181;
 	}
+
 </style>
+`
