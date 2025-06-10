137e2378865892deec8c0f21f2e1c7ef6de6d92a
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-06-11 00:01:03 +0400

    .

--- src/links.svelte
+++ src/links.svelte
@@ -5,22 +5,22 @@
 
 <div id="yes">
 	<Link
-		link="#/"
+		link="/"
 		text="Home"
 		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/4c0390f6c9a91b83f3ae6da9d5b5a75181472fd0_home_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
 	/>
 	<Link
-		link="#/aboutme"
+		link="/aboutme"
 		text="About"
 		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/978be0e162f6aebd63c1237006f065030a90264e_person_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
 	/>
 	<Link
-		link="#/contactme"
+		link="/contactme"
 		text="Contact"
 		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/fc3f312e696ec561d201c3ffe0874df1e09e70f0_waving_hand_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
 	/>
 	<Link
-		link="#/Projects"
+		link="/projects"
 		text="Projects"
 		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/7b8b6bf02ed987b0e68ce5c2814ea408d666caf5_folder_code_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
 	/>
@@ -32,19 +32,19 @@
 </div>
 <div id="no">
 	<LinkMob
-		link="#/"
+		link="/"
 		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/4c0390f6c9a91b83f3ae6da9d5b5a75181472fd0_home_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
 	/>
 	<LinkMob
-		link="#/aboutme"
+		link="/aboutme"
 		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/978be0e162f6aebd63c1237006f065030a90264e_person_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
 	/>
 	<LinkMob
-		link="#/contactme"
+		link="/contactme"
 		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/fc3f312e696ec561d201c3ffe0874df1e09e70f0_waving_hand_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
 	/>
 	<LinkMob
-		link="#/Projects"
+		link="/projects"
 		logo="https://hc-cdn.hel1.your-objectstorage.com/s/v3/7b8b6bf02ed987b0e68ce5c2814ea408d666caf5_folder_code_24dp_e3e3e3_fill0_wght400_grad0_opsz24.svg"
 	/>
 	<LinkMob
