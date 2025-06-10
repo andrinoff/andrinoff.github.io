9bd88f4ea94accddf553c53dd0d4f86ecbc5d83a
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-06-10 23:09:30 +0400

    fix: possibly

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,17 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: "404.html",
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+		}
+	}
 }
