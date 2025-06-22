71152bc260c75ef50ca09963d70dd83c7cd47f2a
drew <realandrinoff@gmail.com>
2025-06-22 15:26:47 +0300

    Added mastodon and verification

--- src/routes/aboutme/+page.svelte
+++ src/routes/aboutme/+page.svelte
@@ -1,82 +1,107 @@
 <script>
-	import Techstack from "./techstack.svelte";
-	</script>
+  import Techstack from "./techstack.svelte";
+</script>
 
 <div id="root">
-	<h1 class="text-3xl md:text-4xl font-bold text-center mb-2 "> Education:</h1>
-	<br />
-		<p>
-			2024-2026: A-levels </p>
-		<p id ="lower"> CS, Math, Econ at Projector School.</p>
-		<br/>
-	<h1 class="text-3xl md:text-4xl font-bold text-center mb-2 ">Experience:</h1>
-	<div class="grid grid-cols-[40px_1fr] gap-x-2 px-4">
-            <div class="flex flex-col items-center gap-1 pt-3">
-            <div class="text-[#101418]" data-icon="Buildings" data-size="24px" data-weight="regular">
-                <svg width="20" height="20">
-				<circle cx="10" cy="10" r="5" fill="white" />
-				</svg>
-            </div>
-            <div class="w-[1.5px] bg-[#d4dbe2] h-2 grow"></div>
-            </div>
-            <div class="flex flex-1 flex-col py-3">
-            <p class="text-[#fff] text-base font-medium leading-normal">Studio Fullstack Engineer</p>
-            <p class="text-[#5c738a] text-base font-normal leading-normal">2023-2024</p>
-            </div>
-            <div class="flex flex-col items-center gap-1">
-            <div class="w-[1.5px] bg-[#d4dbe2] h-2"></div>
-            <div class="text-[#101418]" data-icon="RocketLaunch" data-size="24px" data-weight="regular">
-                <svg width="20" height="20">
-				<circle cx="10" cy="10" r="5" fill="white" />
-				</svg>	
-            </div>
-            <div class="w-[1.5px] bg-[#d4dbe2] h-2 grow"></div>
-            </div>
-            <div class="flex flex-1 flex-col py-3">
-            <p class="text-[#fff] text-base font-medium leading-normal">Freemoviesfull.cc Backend Engineer</p>
-            <p class="text-[#5c738a] text-base font-normal leading-normal">2024-2025</p>
-            </div>
-			<div class="flex flex-col items-center gap-1">
-            <div class="w-[1.5px] bg-[#d4dbe2] h-2"></div>
-            <div class="text-[#101418]" data-icon="RocketLaunch" data-size="24px" data-weight="regular">
-                <svg width="20" height="20">
-				<circle cx="10" cy="10" r="5" fill="white" />
-				</svg>	
-            </div>
-			
-            <div class=" bg-[#d4dbe2] h-2 grow"></div>
-            </div>
-            <div class="flex flex-1 flex-col py-3">
-            <p class="text-[#fff] text-base font-medium leading-normal">Freelance Fullstack Engineer</p>
-            <p class="text-[#5c738a] text-base font-normal leading-normal">2022-present</p>
-            </div>
-        </div>
-		<br/>
-		<Techstack />
-		<h1 class="text-3xl md:text-4xl font-bold text-center mb-2">See more:</h1>
-		<div class = "text-center flex space-between justify-center  flex-col">
-		<a class= "underline" href = "https://andrinoff.com/cv">CV</a>
-		<a class= "underline" href="https://andrinoff.com/appreciations">Appreciations</a>
-		</div>
+  <h1 class="text-3xl md:text-4xl font-bold text-center mb-2">Education:</h1>
+  <br />
+  <p>2024-2026: A-levels</p>
+  <p id="lower">CS, Math, Econ at Projector School.</p>
+  <br />
+  <h1 class="text-3xl md:text-4xl font-bold text-center mb-2">Experience:</h1>
+  <div class="grid grid-cols-[40px_1fr] gap-x-2 px-4">
+    <div class="flex flex-col items-center gap-1 pt-3">
+      <div
+        class="text-[#101418]"
+        data-icon="Buildings"
+        data-size="24px"
+        data-weight="regular"
+      >
+        <svg width="20" height="20">
+          <circle cx="10" cy="10" r="5" fill="white" />
+        </svg>
+      </div>
+      <div class="w-[1.5px] bg-[#d4dbe2] h-2 grow"></div>
+    </div>
+    <div class="flex flex-1 flex-col py-3">
+      <p class="text-[#fff] text-base font-medium leading-normal">
+        Studio Fullstack Engineer
+      </p>
+      <p class="text-[#5c738a] text-base font-normal leading-normal">
+        2023-2024
+      </p>
+    </div>
+    <div class="flex flex-col items-center gap-1">
+      <div class="w-[1.5px] bg-[#d4dbe2] h-2"></div>
+      <div
+        class="text-[#101418]"
+        data-icon="RocketLaunch"
+        data-size="24px"
+        data-weight="regular"
+      >
+        <svg width="20" height="20">
+          <circle cx="10" cy="10" r="5" fill="white" />
+        </svg>
+      </div>
+      <div class="w-[1.5px] bg-[#d4dbe2] h-2 grow"></div>
+    </div>
+    <div class="flex flex-1 flex-col py-3">
+      <p class="text-[#fff] text-base font-medium leading-normal">
+        Freemoviesfull.cc Backend Engineer
+      </p>
+      <p class="text-[#5c738a] text-base font-normal leading-normal">
+        2024-2025
+      </p>
+    </div>
+    <div class="flex flex-col items-center gap-1">
+      <div class="w-[1.5px] bg-[#d4dbe2] h-2"></div>
+      <div
+        class="text-[#101418]"
+        data-icon="RocketLaunch"
+        data-size="24px"
+        data-weight="regular"
+      >
+        <svg width="20" height="20">
+          <circle cx="10" cy="10" r="5" fill="white" />
+        </svg>
+      </div>
 
-
-
+      <div class=" bg-[#d4dbe2] h-2 grow"></div>
+    </div>
+    <div class="flex flex-1 flex-col py-3">
+      <p class="text-[#fff] text-base font-medium leading-normal">
+        Freelance Fullstack Engineer
+      </p>
+      <p class="text-[#5c738a] text-base font-normal leading-normal">
+        2022-present
+      </p>
+    </div>
+  </div>
+  <br />
+  <Techstack />
+  <h1 class="text-3xl md:text-4xl font-bold text-center mb-2">See more:</h1>
+  <div class="text-center flex space-between justify-center flex-col">
+    <a class="underline" href="https://andrinoff.com/cv">CV</a>
+    <a class="underline" href="https://andrinoff.com/appreciations"
+      >Appreciations</a
+    >
+  </div>
 </div>
+<a rel="me" href="https://mastodon.social/@andrinoff">Mastodon</a>
 
 <style>
-	@import "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css";
-	h1 {
-		margin-top: 20px;
-	}
-	#root {
-		display: flex;
-		flex-direction: column;
-		
-		align-items: center;
-	}
-	#lower {
-		margin: 0px;
-		color: #818181;
-	}
+  @import "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css";
+  h1 {
+    margin-top: 20px;
+  }
+  #root {
+    display: flex;
+    flex-direction: column;
 
+    align-items: center;
+  }
+  #lower {
+    margin: 0px;
+    color: #818181;
+  }
 </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,224 +1,227 @@
 <script>
   import Link from "../../link.svelte";
 
-    let Projects = [
-        {
-            Name: "Personal Website",
-            Description: "This awesome website about me and my projects!",
-            Image: "https://i.imgur.com/5zHfTeQ.png",
-            Images: ["Javascript", "svelte", "vite"],
-            Link: "https://andrinoff.github.io",
-            Type: "fullstack",
-        },
-        {
-            Name: "Work Progress",
-            Description:
-                "A VS Code extension that shows your work progress in a nice way.",
-            Image: "https://github.com/andrinoff/work-progress.github.io/blob/master/icon.png?raw=true",
-            Images: ["typescript"],
-            Link: "https://github.com/andrinoff/work-progress",
-            Type: "fullstack",
-        },
-        {
-            Name: "Anglobotik",
-            Description:
-                "A Telegram bot that helps you mock English writing exam",
-            Image: "https://github.com/andrinoff/anglobotik/raw/main/docs/icon.jpg",
-            Images: ["python", "telegram", "openai"],
-            Link: "https://github.com/andrinoff/anglobotik",
-            Type: "backend",
-        },
-        {
-            Name: "Currency Converter",
-            Description:
-                "A Telegram bot with multi-language support",
-            Image: "https://www.investopedia.com/thmb/lSMOlzF853E72-0HDkH25mmAdOM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-848755326-48dd2711646247648c4faebc98715119.jpg",
-            Images: ["python", "sqlite", "telegram"],
-            Link: "https://github.com/andrinoff/TelegramCurrencyConverter",
-            Type: "backend",
-        },
-        {
-            Name: "Tbilisi Hack Club Website",
-            Description:
-                "A website for Tbilisi Hack Club",
-            Image: "https://i.imgur.com/bfV8QXt.png",
-            Images: ["svelte", "tailwindcss", ],
-            Link: "https://tbilisi.hackclub.com",
-            Type: "frontend",
-        },
-        {
-            Name: "Bot API",
-            Description: "A customizable API that sends your message in different channels",
-            Image: "https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fzt4zmavxge1lg5u7lkp7.jpg",
-            Images: ["vercel", "go"],
-            Link: "https://github.com/andrinoff/bot-api",
-            Type: "backend",
-        },
-        {
-            Name: "Organization auto-join",
-            Description: "An API that automatically invites you to the set organization team",
-            Image: "https://www.cril.univ-artois.fr/pyxai/assets/figures/github.png",
-            Images: ["javascript", "vercel"],
-            Link: "https://github.com/andrinoff/bot-api",
-            Type: "backend",
-        },
-        {
-            Name: "Infinite Wordle",
-            Description: "A Wordle copy. Was made for a technical interview, and finished afterwards",
-            Image: "https://i.imgur.com/3UTklGw.png",
-            Images: ["html5", "javascript", "css"],
-            Link: "https://wordle.andrinoff.com",
-            Type: "fullstack"
-        },
-        {
-            Name: "Assets Viewer",
-            Description: "A website that helps you view assets in a repository in a user-friendly way",
-            Image: "https://i.imgur.com/sWVh3Cn.png",
-            Images: ["html5", "javascript", "css"],
-            Link: "https://github.com/tbilisihc/assets",
-            Type: "fullstack"
+  let Projects = [
+    {
+      Name: "Personal Website",
+      Description: "This awesome website about me and my projects!",
+      Image: "https://i.imgur.com/5zHfTeQ.png",
+      Images: ["Javascript", "svelte", "vite"],
+      Link: "https://andrinoff.github.io",
+      Type: "fullstack",
+    },
+    {
+      Name: "Work Progress",
+      Description:
+        "A VS Code extension that shows your work progress in a nice way.",
+      Image:
+        "https://github.com/andrinoff/work-progress.github.io/blob/master/icon.png?raw=true",
+      Images: ["typescript"],
+      Link: "https://github.com/andrinoff/work-progress",
+      Type: "fullstack",
+    },
+    {
+      Name: "Anglobotik",
+      Description: "A Telegram bot that helps you mock English writing exam",
+      Image: "https://github.com/andrinoff/anglobotik/raw/main/docs/icon.jpg",
+      Images: ["python", "telegram", "openai"],
+      Link: "https://github.com/andrinoff/anglobotik",
+      Type: "backend",
+    },
+    {
+      Name: "Currency Converter",
+      Description: "A Telegram bot with multi-language support",
+      Image:
+        "https://www.investopedia.com/thmb/lSMOlzF853E72-0HDkH25mmAdOM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-848755326-48dd2711646247648c4faebc98715119.jpg",
+      Images: ["python", "sqlite", "telegram"],
+      Link: "https://github.com/andrinoff/TelegramCurrencyConverter",
+      Type: "backend",
+    },
+    {
+      Name: "Tbilisi Hack Club Website",
+      Description: "A website for Tbilisi Hack Club",
+      Image: "https://i.imgur.com/bfV8QXt.png",
+      Images: ["svelte", "tailwindcss"],
+      Link: "https://tbilisi.hackclub.com",
+      Type: "frontend",
+    },
+    {
+      Name: "Bot API",
+      Description:
+        "A customizable API that sends your message in different channels",
+      Image:
+        "https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fzt4zmavxge1lg5u7lkp7.jpg",
+      Images: ["vercel", "go"],
+      Link: "https://github.com/andrinoff/bot-api",
+      Type: "backend",
+    },
+    {
+      Name: "Organization auto-join",
+      Description:
+        "An API that automatically invites you to the set organization team",
+      Image: "https://www.cril.univ-artois.fr/pyxai/assets/figures/github.png",
+      Images: ["javascript", "vercel"],
+      Link: "https://github.com/andrinoff/bot-api",
+      Type: "backend",
+    },
+    {
+      Name: "Infinite Wordle",
+      Description:
+        "A Wordle copy. Was made for a technical interview, and finished afterwards",
+      Image: "https://i.imgur.com/3UTklGw.png",
+      Images: ["html5", "javascript", "css"],
+      Link: "https://wordle.andrinoff.com",
+      Type: "fullstack",
+    },
+    {
+      Name: "Assets Viewer",
+      Description:
+        "A website that helps you view assets in a repository in a user-friendly way",
+      Image: "https://i.imgur.com/sWVh3Cn.png",
+      Images: ["html5", "javascript", "css"],
+      Link: "https://github.com/tbilisihc/assets",
+      Type: "fullstack",
+    },
+  ];
 
-        }
-
-    ];
-
-    // Get unique types
-    let types = [...new Set(Projects.map(p => p.Type))];
+  // Get unique types
+  let types = [...new Set(Projects.map((p) => p.Type))];
 </script>
 
 <div id="root">
-    <h1>My Projects</h1>
-    {#each types as type}
-        <h2 style="margin-top:2em">{type.charAt(0).toUpperCase() + type.slice(1)} Projects</h2>
-        <div id="grid">
-            {#each Projects.filter(p => p.Type === type) as Project}
-                <div class="project-card">
-                    <div class="card-content">
-                        {#if Project.Name}
-                            <h2>{Project.Name}</h2>
-                        {/if}
-                        <p>{@html Project.Description}</p>
-                    </div>
-                    <div class="image-gallery">
-                        {#each Project.Images as Image}
-                            <img
-                                class="gallery-image"
-                                alt="Project Tech Stack"
-                                src="https://cdn.simpleicons.org/{Image}/grey"
-                            />
-                        {/each}
-                    </div>
-                    <div class="image-cover">
-                        <img class="main-image" src={Project.Image} alt="Project" />
-                    </div>
-                    <a href={Project.Link} target="_blank" class="project-link">
-                        <button class="ViewProject">Open Project</button>
-                    </a>
-                </div>
+  <h1>My Projects</h1>
+  {#each types as type}
+    <h2 style="margin-top:2em">
+      {type.charAt(0).toUpperCase() + type.slice(1)} Projects
+    </h2>
+    <div id="grid">
+      {#each Projects.filter((p) => p.Type === type) as Project}
+        <div class="project-card">
+          <div class="card-content">
+            {#if Project.Name}
+              <h2>{Project.Name}</h2>
+            {/if}
+            <p>{@html Project.Description}</p>
+          </div>
+          <div class="image-gallery">
+            {#each Project.Images as Image}
+              <img
+                class="gallery-image"
+                alt="Project Tech Stack"
+                src="https://cdn.simpleicons.org/{Image}/grey"
+              />
             {/each}
+          </div>
+          <div class="image-cover">
+            <img class="main-image" src={Project.Image} alt="Project" />
+          </div>
+          <a href={Project.Link} target="_blank" class="project-link">
+            <button class="ViewProject">Open Project</button>
+          </a>
         </div>
-    {/each}
+      {/each}
+    </div>
+  {/each}
 </div>
 
 <style>
-    #root {
-        display: flex;
-        flex-direction: column;
-        margin: 0;
-        align-items: center;
-        padding: 0 10px; /* Add some padding for small screens */
-    }
-    h2,
-    p {
-        margin: 0;
-        padding: 5px;
-    }
+  #root {
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    align-items: center;
+    padding: 0 10px; /* Add some padding for small screens */
+  }
+  h2,
+  p {
+    margin: 0;
+    padding: 5px;
+  }
 
-    p {
-        color: #818181;
-        text-align: center;
-    }
+  p {
+    color: #818181;
+    text-align: center;
+  }
+  #grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    max-width: 1200px;
+    justify-content: center;
+  }
+
+  .project-card {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    border: 3px solid #333;
+    width: 250px;
+    margin: 10px;
+    padding: 5px;
+    padding-bottom: 0px;
+  }
+
+  .card-content {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .image-gallery {
+    display: flex;
+    gap: 10px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+  .gallery-image {
+    width: 30px;
+    height: 30px;
+    object-fit: cover;
+  }
+  .image-cover {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+  .main-image {
+    height: 146px;
+    max-width: 246px;
+    text-align: center;
+    border: 2px solid #2e2e2e;
+  }
+  .project-link {
+    width: 100%;
+  }
+  .ViewProject {
+    background-color: #242424;
+    color: white;
+    border: none;
+    margin-bottom: 5px;
+    padding: 10px;
+    width: 100%;
+    cursor: pointer;
+    font-size: 16px;
+    font-family: "Funnel Display", sans-serif;
+  }
+  .ViewProject:hover {
+    background-color: #333;
+  }
+
+  /* --- Styles for Tablets --- */
+  @media (max-width: 1024px) {
     #grid {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        gap: 20px;
-        max-width: 1200px;
-        justify-content: center;
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
 
+  /* --- Styles for Mobile Phones --- */
+  @media (max-width: 768px) {
+    #grid {
+      grid-template-columns: 1fr;
     }
-
-    .project-card {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        border: 3px solid #333;
-        width: 250px;
-        margin: 10px;
-        padding: 5px;
-        padding-bottom: 0px;
-    }
-
-    .card-content {
-        flex-grow: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .image-gallery {
-        display: flex;
-        gap: 10px;
-        margin-top: 10px;
-        margin-bottom: 10px;
-    }
-    .gallery-image {
-        width: 30px;
-        height: 30px;
-        object-fit: cover;
-    }
-    .image-cover {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-bottom: 10px;
-    }
-    .main-image {
-        height: 146px;
-        max-width: 246px;
-        text-align: center;
-        border: 2px solid #2e2e2e;
-    }
-    .project-link {
-        width: 100%;
-    }
-    .ViewProject {
-        background-color: #242424;
-        color: white;
-        border: none;
-        margin-bottom: 5px;
-        padding: 10px;
-        width: 100%;
-        cursor: pointer;
-        font-size: 16px;
-        font-family: "Funnel Display", sans-serif;
-    }
-    .ViewProject:hover {
-        background-color: #333;
-    }
-
-    /* --- Styles for Tablets --- */
-    @media (max-width: 1024px) {
-        #grid {
-            grid-template-columns: repeat(2, 1fr);
-        }
-    }
-
-    /* --- Styles for Mobile Phones --- */
-    @media (max-width: 768px) {
-        #grid {
-            grid-template-columns: 1fr;
-        }
-    }
+  }
 </style>
