7fa16bccaa945627e6643587a77910f275e070b2
drew <realandrinoff@gmail.com>
2025-06-27 15:40:49 +0400

    Changed specialization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,7 +48,8 @@
             <div class="profile-text">
                 <h1 class="name-heading">Drew Smirnoff</h1>
                 <p class="bio">
-                    Software Engineer <span class="divider">|</span> Based in Tbilisi
+                    Full stack developer <span class="divider">|</span> Based in
+                    Tbilisi
                 </p>
             </div>
         </section>
